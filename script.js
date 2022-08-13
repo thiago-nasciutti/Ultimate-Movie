1b3e2238980b21df07addf0e51066b93a7aa31c5
thiago <nasciutti.thiago@gmail.com>
2022-08-13 17:43:59 -0400

    plot&poster

---
+++ script.js
@@ -0,0 +1,21 @@
+function search(){
+    let title = document.getElementById("title").value
+
+    //Get url 1 to access OMDDb API 
+    let url1 = "https://www.omdbapi.com/?t="+title+"&apikey=db0da826"
+    fetch(url1)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+            //Plot
+            let plot = data.Plot
+            document.getElementById("plot").innerHTML = plot
+            let poster = data.Poster
+
+            
+            document.getElementById("poster").src = poster
+        })
+
+
+}
