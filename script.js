da516c9a16875ebfa507ba16af7da5f7ecaaccaf
thiago <nasciutti.thiago@gmail.com>
2022-08-14 11:42:09 -0400

    rotten_tomatoes

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function search(){
     let title = document.getElementById("movieInput").value
-
-    //Get url 1 to access OMDDb API 
+  
+    //Get url 1 to access OMDb-API 
     let url1 = "https://www.omdbapi.com/?t="+title+"&apikey=db0da826"
+   
     fetch(url1)
         .then(function(response){
             return response.json()
@@ -20,7 +21,23 @@ function search(){
             //Poster
             let poster = data.Poster
             document.getElementById("poster").src = poster
-        })
 
+            //imdbID
+            let imdbID = data.imdbID
+
+            //Get url 2 to access IMDb-API
+            let url2 = "https://imdb-api.com/en/API/Ratings/k_vnngt5ar/"+imdbID
+                fetch(url2)
+                .then(function(response){
+                    return response.json()
+                })
+                .then(function(data){
+        
+                    //Rotten Tomatoes
+                    let rotten= data.rottenTomatoes
+                    document.getElementById("rotten").innerHTML = "Rotten Tomatoes: "+ rotten + "%"
+                })
+        })
+           
 
 }
